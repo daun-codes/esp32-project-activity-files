eb9dd068bb108338e0c7f094436a50c6fc448ed8
daun-codes <jervickamodia@gmail.com>
2026-08-31 05:24:29 +0800

    Added Activity 5 - Non blocking timer with push button

---
+++ Activity-05-Non-Blocking-Millis/README.md
@@ -0,0 +1,19 @@
+# Activity 5 - Non Blocking Timing with Push Button
+
+## Description 
+
+This activity has two leds that has different way of blinking. The blue led lits for every one second continuously and while the blue led will only blink when push button was pressed. The blue led will still continue blinking regardless the button was pressed or not. This demonstrate how different components can run without affecting the others with the use of the built in system clock called 'millis'.
+
+## Components Used
+
+
+- 1 pcs ESP32 MCU
+- 2 pcs LED (blue, yellow)
+- 2 pcs 330 ohms Resistor
+- 1 pcs tactile switch / push button
+- jumper wires
+
+
+## ScreenShot
+
+- ![PWM Brightness Simulation](../img/Activity-05.png)
---
+++ Activity-05-Non-Blocking-Millis/src/main.cpp
@@ -0,0 +1,42 @@
+#include <arduino.h>
+
+
+constexpr uint8_t YELLOW_LED_PIN = 19;
+constexpr uint8_t BLUE_LED_PIN = 18;
+constexpr uint8_t BUTTON_PIN = 12;
+
+const uint16_t timeInterval = 1000;
+
+unsigned long prevTime = 0;
+
+bool ledState = false;
+
+void setup() {
+  pinMode(YELLOW_LED_PIN, OUTPUT);
+  pinMode(BLUE_LED_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+
+}
+
+void loop() {
+  unsigned long currTime = millis();
+  int buttonSensor = digitalRead(BUTTON_PIN);
+
+
+  if (currTime - prevTime >= timeInterval) {
+    
+    prevTime = currTime;
+    ledState = !ledState;
+    digitalWrite(BLUE_LED_PIN, ledState);
+  }
+
+  if (buttonSensor == LOW) {
+    digitalWrite(YELLOW_LED_PIN, HIGH);
+  }
+
+  else {
+    digitalWrite(YELLOW_LED_PIN, LOW);
+  }
+
+
+}
