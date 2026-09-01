692bf5609c766e65f28d2e3560054eb449d54e2d
donanalyst <jervickamodia@gmail.com>
2026-09-02 05:44:11 +0800

    Optimized the code | Removed the emergency stop variable | Added print display for countdown reset

--- Activity-06-Emergency-Stop-Interrupt/src/main.cpp
+++ Activity-06-Emergency-Stop-Interrupt/src/main.cpp
@@ -10,10 +10,13 @@ volatile unsigned long prevTime = 0;
 volatile bool systemTripped = false;
 
 constexpr uint16_t greenTime = 1000;
-constexpr uint16_t timeReset = 2000;
-bool greenLedOn = false;
-bool emergencyStop = false;
+constexpr uint16_t timeReset = 5000;
+constexpr uint16_t resetCountdown = 1000;
 
+
+int resetCounter = 0;
+bool greenLedOn = false;
+bool displayCountdown = true;
 
 void emergencyState() {
   
@@ -25,10 +28,9 @@ void emergencyState() {
 
 
 void normalState(bool ledState) {
-  if (emergencyStop == false) {
-    digitalWrite(GREEN_LED_PIN, ledState);
-    digitalWrite(RED_LED_PIN, LOW);
-  }
+  digitalWrite(GREEN_LED_PIN, ledState);
+  digitalWrite(RED_LED_PIN, LOW);
+
 }
 
 
@@ -62,7 +64,6 @@ void loop() {
 
   if (systemTripped == false) {
     
-    emergencyStop = false;
     if (currTime - prevTime >= greenTime) {
     prevTime = currTime;
 
@@ -76,11 +77,33 @@ void loop() {
   else if (systemTripped == true) {
     
     emergencyState();
+  
 
-    if (currTime - prevTime >= timeReset) {
-      systemTripped = false;
-      emergencyStop = true;
+    if (currTime - prevTime >= resetCountdown) {
+      
+      
+      if (displayCountdown == true) {
+        Serial.println("");
+        Serial.println("EMERGENCY TRIGGERED!");
+        Serial.println("RESET IN 5 SECONDS");
+        displayCountdown = false;
+      }
+
+      Serial.print("RESETTING IN: ");
+      Serial.println((timeReset - resetCounter) / 1000);
+      resetCounter = resetCounter + 1000;
+
+      if (resetCounter >= timeReset) {
+        displayCountdown = true;
+        resetCounter = 0;
+        systemTripped = false;
+        
+      }
+    
       prevTime = currTime;
+
+      
+
     }
   }
 }
